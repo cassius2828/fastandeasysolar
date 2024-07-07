16e3573f7031d2de448f47bdd44531a54de7ec7f
Cassius <cassius.reynolds.dev@gmail.com>
2024-07-07 16:31:38 -0700

    set mobile display state when you choose a link in the mobile nav

--- src/components/Nav/MobileNav.jsx
+++ src/components/Nav/MobileNav.jsx
@@ -1,22 +1,27 @@
-export const MobileNav = ({scrollToTop}) => {
+export const MobileNav = ({ scrollToTop, setIsOpen }) => {
   return (
     <ul
       id="mobile-nav"
       className="lg:hidden absolute top-0 left-0 z- w-screen h-screen bg-blue-900 text-gray-200 capitalize flex flex-col items-center justify-around text-2xl"
     >
-      <li onClick={scrollToTop}>
+      <li
+        onClick={() => {
+          setIsOpen(false);
+          scrollToTop();
+        }}
+      >
         <a href="">home</a>
       </li>
-      <li>
+      <li onClick={() => setIsOpen(false)}>
         <a href="#scroll-to-services">services</a>
       </li>
-      <li>
+      <li onClick={() => setIsOpen(false)}>
         <a href="#scroll-to-programs">programs</a>
       </li>
-      <li>
+      <li onClick={() => setIsOpen(false)}>
         <a href="#contact">contact</a>
       </li>{" "}
-      <li>
+      <li onClick={() => setIsOpen(false)}>
         <a href="#about">about</a>
       </li>
     </ul>
--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -37,7 +37,7 @@ const Nav = () => {
       </ul>
    
       <Hamburger isOpen={isOpen} setIsOpen={setIsOpen} />
-      {isOpen && <MobileNav scrollToTop={scrollToTop} />}
+      {isOpen && <MobileNav setIsOpen={setIsOpen} scrollToTop={scrollToTop} />}
    
     </nav>
   );
