1e8ea04117096fe763a7250467a1d4a23acb1e32
Cassius <cassius.reynolds.dev@gmail.com>
2024-07-07 16:39:38 -0700

    removed animation from mobile background by adding a custom hook for getting the viewport size

--- src/components/Background/index.jsx
+++ src/components/Background/index.jsx
@@ -1,25 +1,28 @@
-// src/components/Landing.jsx
 import React from "react";
-import { ParallaxBanner } from "react-scroll-parallax";
-
+import useWindowWidth from "../../customHooks/useWindowWidth";
 const Background = () => {
+  const windowWidth = useWindowWidth();
+
   return (
     <section
       id="Background"
-      className="h-screen w-screen flex items-center justify-center fixed  top-0 z-20 bg-[#130a28]"
+      className="h-screen w-screen flex items-center justify-center fixed top-0 z-20 bg-[#130a28]"
     >
       <div className="absolute z-0 h-full w-full inset-0 bg-gradient-vertical fade-in-and-up-sky"></div>
       <picture className="absolute z-0 h-full w-full">
-        <source
-          className="absolute top-0 left-0"
-          srcSet="/images/city-bg-tall.webp"
-          media="(max-width: 599px)"
-        />
-        <img
-          className="fade-in-and-up-land absolute top-0"
-          src="/images/city-bg-no-sky.webp"
-          alt="City Background"
-        />
+        {windowWidth <= 599 ? (
+          <img
+            className="absolute top-0 left-0"
+            src="/images/city-bg-tall.webp"
+            alt="City Background for small screens"
+          />
+        ) : (
+          <img
+            className="fade-in-and-up-land absolute top-0 left-0"
+            src="/images/city-bg-no-sky.webp"
+            alt="City Background for large screens"
+          />
+        )}
       </picture>
 
       <div className="absolute w-full h-full bg-gray-800 opacity-40"></div>
