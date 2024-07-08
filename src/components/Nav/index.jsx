a1bce1e14e8b3b6d85f1390b2e3bd8529480cb1b
Cassius <cassius.reynolds.dev@gmail.com>
2024-07-07 17:22:25 -0700

    added new logo and text for nav

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -4,20 +4,23 @@ import { Hamburger } from "./Hamburger";
 import { MobileNav } from "./MobileNav";
 
 const Nav = () => {
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
   const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
   return (
     <nav
       id="nav"
-      className=" bg-gray-100 shadow-lg p-0 m-0 fixed z-40 w-full flex items-center justify-end h-52"
+      className=" bg-gray-100 shadow-lg p-0 m-0 fixed z-40 w-full flex items-center justify-between"
     >
-      <img
-        className=" w-96 absolute left-0 "
-        src="/images/logo.svg"
-        alt="logo"
-      />
+      <div className="flex items-center gap-12">
+        <img className="w-40 ml-10 lg:ml-40" src="/images/logo-notext.svg" alt="logo" />
+        <h2 className=" text-3xl lg:text-6xl text-blue-900 uppercase" style={{ textShadow: "2px 2px 0 #93C5FD" }}>
+  Fast and easy solar
+</h2>
+
+      </div>
+
       <ul className=" space-x-4 text-blue-900 mr-48 text-2xl gap-12 capitalize hidden lg:flex">
         <li onClick={scrollToTop}>
           <a href="">home</a>
@@ -35,14 +38,11 @@ const Nav = () => {
           <a href="#about">about</a>
         </li>
       </ul>
-   
+
       <Hamburger isOpen={isOpen} setIsOpen={setIsOpen} />
       {isOpen && <MobileNav setIsOpen={setIsOpen} scrollToTop={scrollToTop} />}
-   
     </nav>
   );
 };
 
 export default Nav;
-
-
