314ba2b5a35466c29560015b7cb59cc62f528561
Cassius <cassius.reynolds.dev@gmail.com>
2024-07-07 17:45:09 -0700

    changed spacing for mobile nav to allow more space at the bottom

--- src/components/Nav/MobileNav.jsx
+++ src/components/Nav/MobileNav.jsx
@@ -2,9 +2,9 @@ export const MobileNav = ({ scrollToTop, setIsOpen }) => {
   return (
     <ul
       id="mobile-nav"
-      className="lg:hidden absolute top-0 left-0 z- w-screen h-screen bg-blue-900 text-gray-200 capitalize flex flex-col items-center justify-around text-2xl"
+      className="lg:hidden absolute top-0 left-0 z- w-screen h-screen bg-blue-900 text-gray-200 capitalize flex flex-col items-center justify-start gap-32 text-2xl"
     >
-      <li
+      <li className="mt-40"
         onClick={() => {
           setIsOpen(false);
           scrollToTop();
