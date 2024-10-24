722900988ae1faf9554030126712c7d9e326c7d4
Cassius <cassius.reynolds.dev@gmail.com>
2024-10-24 01:58:39 -0700

    added explicit custom style for -translate-y using the box and rem to ensure the style makes it to production

--- src/components/Nav/Hamburger.jsx
+++ src/components/Nav/Hamburger.jsx
@@ -9,29 +9,32 @@ export const Hamburger = ({ isOpen, setIsOpen }) => {
       className="flex flex-col justify-between items-center lg:hidden fixed z-50 top-10 right-5 h-6"
     >
       <span
-      style={{height:'.125rem'}}
+        style={{ height: ".125rem" }}
         className={`block transition-all duration-300 ease-out 
              w-12 rounded-sm ${
-              isOpen
-                ? "rotate-45 translate-y-3 bg-gray-200 "
-                : "-translate-y-0.5 bg-blue-700 "
-            }`}
+               isOpen
+                 ? "rotate-45 translate-y-3 bg-gray-200 "
+                 : "-translate-y-0.5 bg-blue-700 "
+             }`}
       ></span>
       <span
-      style={{height:'.125rem'}}
+        style={{ height: ".125rem" }}
         className={`block transition-all duration-300 ease-out 
              w-12 rounded-sm my-0.5 ${
-              isOpen ? "opacity-0 bg-gray-200 " : "opacity-100 bg-blue-700 "
-            }`}
+               isOpen ? "opacity-0 bg-gray-200 " : "opacity-100 bg-blue-700 "
+             }`}
       ></span>
       <span
-      style={{height:'.125rem'}}
+        style={{
+          height: ".125rem",
+
+        }}
         className={`block transition-all duration-300 ease-out 
              w-12 rounded-sm ${
-              isOpen
-                ? "-rotate-45 -translate-y-2.5 bg-gray-200 "
-                : "translate-y-0.5 bg-blue-700 "
-            }`}
+               isOpen
+                 ? "-rotate-45 -translate-y-[.625rem] bg-gray-200 "
+                 : "translate-y-0.5 bg-blue-700 "
+             }`}
       ></span>
     </button>
   );
