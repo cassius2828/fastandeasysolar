dffcbeba8d13c5237f816d7d1f38fa4593e776b5
Cassius <cassius.reynolds.dev@gmail.com>
2024-10-24 00:52:20 -0700

    updated safelist to only include index.css file instead of explicitly stating all names

--- postcss.config.js
+++ postcss.config.js
@@ -6,36 +6,13 @@ export default {
   plugins: [
     isProduction &&
       purgecss({
-        content: ["./src/**/*.html", "./src/**/*.jsx", "./src/**/*.js"], // Adjust based on your file structure
-        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-        safelist: [
-          'col-span-2',
-          'flex',
-          'flex-row',
-          'gap-12',
-          'h-',
-          'hidden',
-          'mb-0',
-          'ml-0',
-          'ml-40',
-          'mr-0',
-          'mr-12',
-          'mr-8',
-          'mt-0',
-          'mt-48',
-          'pl-20',
-          'pr-40',
-          'text-2xl',
-          'text-3xl',
-          'text-4xl',
-          'text-6xl',
-          'text-8xl',
-          'top-56',
-          'w-1/2',
-          'w-1/3',
-          'w-9/12',
-          'w-full',
+        content: [
+          "./src/**/*.html",
+          "./src/**/*.jsx",
+          "./src/**/*.js",
+          "./src/index.css" // Add index.css to the content array
         ],
+        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
       }),
   ].filter(Boolean),
 };
