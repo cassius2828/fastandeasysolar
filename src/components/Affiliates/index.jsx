99f513307332b6737083dbbb3cc0a598f3cd2df0
Cassius <cassius.reynolds.dev@gmail.com>
2024-11-05 19:16:01 -0800

    added requested changes to website. Commented out the careers form, added the other tesla logo about power installers from v3electric site, and changed text of partnered with to certified partners and installers

--- src/components/Affiliates/index.jsx
+++ src/components/Affiliates/index.jsx
@@ -17,10 +17,10 @@ const GoodLeapLogo = `${
 const EnFinLogo = `${
   import.meta.env.VITE_S3_OBJECT_BASE_URL
 }faes/logos/enfin.webp`;
-
+const TeslaLogo2 = 'https://v3electric.com/static/images/tesla_logo.png'
 const Affiliates = () => {
   const affiliates = [
-    { name: "Tesla", logo: TeslaLogo },
+    { name: "Tesla", logo: TeslaLogo2 },
     { name: "Sunrun", logo: SunrunLogo },
     { name: "Everbright", logo: EverbrightLogo },
     { name: "Sunnova", logo: SunnovaLogo },
@@ -31,7 +31,7 @@ const Affiliates = () => {
 
   return (
     <div className="w-full flex flex-col items-center justify-center py-20 px-3 bg-gray-100 relative z-20">
-      <h2 className="text-5xl font-bold mb-8 text-blue-900">Partnered With</h2>
+      <h2 className="text-5xl font-bold mb-8 text-blue-900">Certified Partners and Installers</h2>
       <div className="grid grid-cols-2 md:grid-cols-5 grid-rows-3 md:grid-rows-1 w-full place-items-center">
         {affiliates.map((affiliate, index) => (
           <div key={index} className="flex flex-col items-center">
